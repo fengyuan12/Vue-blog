efc13b89a184fd64a337643710803111516a7878
fengyuan <2938085638@qq.com>
2018-11-14 19:40:35 +0800

    token有效期。。。登出。。。

--- server/Controller/UserController.js
+++ server/Controller/UserController.js
@@ -15,7 +15,7 @@ import createToken from '../utils/createToken'
 
 class UserController{
     //登录的处理逻辑在这里
-    async login(ctx){
+    async login(ctx) {
         // console.log(ctx.request.body.user)
         // console.log(ctx.request.body.password)
         let user = ctx.request.body.user
@@ -25,34 +25,36 @@ class UserController{
         //检查用户名是否存在
         //所有的对于数据库的操作，我们不在控制器里面完成，我们在数据库直接封装成一个方法
         const res = (await User.getUserByName(user))[0]
-        if(res){
+        if (res) {
             //如果存在
             //检查密码是否正确
-            if(res.password === md5(password)){
+            if (res.password === md5(password)) {
                 //生成token
                 let token = createToken(res.user)
                 //最后返回token
-                ctx.body={
-                    success:true,
-                    message:'登录成功...',
-                    token:token
+                ctx.body = {
+                    success: true,
+                    message: '登录成功...',
+                    token: token
                 }
-            }else{
+            } else {
                 ctx.body = {
                     success: false,
                     message: '密码错误，请重新填写'
                 }
             }
-        }else{
+        } else {
             ctx.body = {
-                success:false,
-                message:'用户名不存在，请重新填写.....',
+                success: false,
+                message: '用户名不存在，请重新填写.....',
             }
         }
-
-
-
-
+    }
+    async logout(ctx){
+        ctx.body={
+            success:true,
+            message:'退出成功'
+        }
     }
 }
 export default new UserController()
